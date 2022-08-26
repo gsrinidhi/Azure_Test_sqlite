9a2bf5ecdb922e324a10814d58253c35a5cfe8dd
gsrinidhi <gonahalsrinidhi@gmail.com>
2022-08-26 19:56:06 +0530

    COmmit after adding sqlitedb

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql_db1',
-        'USER': 'ServerAlpha',
-        'PASSWORD': 'AlphaGo888',
-        'HOST': 'mysqlserver888.mysql.database.azure.com',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
